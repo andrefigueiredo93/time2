36ba1d35a00cf36c710f27da8300366b224bfd3b
= <=>
2024-08-23 23:11:02 +0200

    wip

--- firebase.js
+++ firebase.js
@@ -24,3 +24,54 @@ function addComment(message) {
     var commentElement = document.querySelector('.comment');
     commentElement.appendChild(newParagraph);
 }
+//function to add comment on database
+function addCommentToDatabase(comment) {
+    //var comment = document.getElementById("comment").value;
+    db.collection("comments").add({
+        message: comment
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            addComment(comment);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+}
+
+// Seleciona o formulário
+var form = document.getElementById('myForm');
+
+// Adiciona um ouvinte de evento para o envio do formulário
+form.addEventListener('submit', function (event) {
+    // Previne o comportamento padrão de envio do formulário
+    event.preventDefault();
+
+    // Obtém os valores dos campos de entrada
+    var comment = document.getElementById('comment').value;
+    addCommentToDatabase(comment);
+
+    form.reset();
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Obtém o modal
+    var modal = document.getElementById("myModal");
+
+    // Obtém o elemento que fecha o modal
+    var span = document.getElementById("closeModal");
+
+    // Quando o usuário clica em <span> (x), fecha o modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    // Quando o usuário clica em qualquer lugar fora do modal, fecha o modal
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
